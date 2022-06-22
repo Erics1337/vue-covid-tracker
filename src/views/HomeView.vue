94395786f8567890fd7cdd6aceb542add11445a9
Eric <erics1337@gmail.com>
2022-06-22 13:23:23 -0600

    Fetching Data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,5 +9,16 @@ import HelloWorld from '@/components/Header.vue'
 export default {
 	name: 'HomeView',
 	components: {},
+	methods: {
+		async fetchCovidData() {
+			const response = await fetch('https://covid19api.com/summary')
+			const data = await response.json()
+			console.log(data)
+		},
+	},
+	async created() {
+		const data = await this.fetchCovidData()
+		console.log(data)
+	},
 }
 </script>

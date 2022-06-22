8211460ce645152f059ce406f2b3f9e459bf1f69
Eric <erics1337@gmail.com>
2022-06-22 12:53:49 -0600

    Setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="home">Hello World</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/Header.vue'
+
+export default {
+	name: 'HomeView',
+	components: {},
+}
+</script>
